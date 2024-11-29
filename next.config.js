519b26b6c60e5163d89eb962f9b7bb9b16bdd357
AI-Driven Coder <aidrivencoder@gmail.com>
2024-11-29 11:28:54 +0800

    Commit from Bolt to GitHub

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['github.com', 'avatars.githubusercontent.com'],
   },
+  experimental: {
+    optimizeCss: true,
+    turbo: {
+      rules: {
+        '*.md': ['raw-loader']
+      }
+    },
+  },
   webpack: (config) => {
+    // Add cache configuration
+    config.cache = {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename],
+      }
+    }
+
     config.module.rules.push({
       test: /\.js$/,
       include: /node_modules\/undici/,
@@ -15,7 +32,7 @@ const nextConfig = {
           plugins: ['@babel/plugin-proposal-private-methods', '@babel/plugin-proposal-class-properties']
         }
       }
-    });
+    })
     return config;
   }
 }
